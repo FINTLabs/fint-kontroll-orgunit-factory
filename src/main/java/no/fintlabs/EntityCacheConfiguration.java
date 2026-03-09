e67ade4884dfbe27951587e54b3f7482690cd46c
chadaj <agata.chadaj@novari.no>
2026-03-09 09:24:22 +0100

    updated fint datamodel to 4.0.10

--- src/main/java/no/fintlabs/EntityCacheConfiguration.java
+++ src/main/java/no/fintlabs/EntityCacheConfiguration.java
@@ -1,6 +1,6 @@
 package no.fintlabs;
 
-import no.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource;
+import no.novari.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource;
 import no.fintlabs.cache.FintCache;
 import no.fintlabs.cache.FintCacheManager;
 import no.fintlabs.orgUnit.OrgUnit;
